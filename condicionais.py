5fb8121b1ec2564c725e5ae6d4878888e231f91a
Eduardo <edu.mneto@hotmail.com>
2025-01-28 01:17:02 -0300

    reorganizando

---
+++ condicionais.py
@@ -0,0 +1,99 @@
+# ===========================
+#  Trabalhando com Instruções if
+# ===========================
+
+# Lista de carros
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+# ===========================
+#  Igualdade
+# ===========================
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())  # Em maiúsculas se for 'bmw'
+    else:
+        print(car.title())  # Primeira letra maiúscula para os demais
+
+# ===========================
+#  Diferença
+# ===========================
+
+for car in cars:
+    if car != 'bmw':
+        print(car.upper())  # Em maiúsculas se não for 'bmw'
+    else:
+        print(car.title())  # Primeira letra maiúscula para 'bmw'
+
+# ===========================
+#  Múltiplas Condições
+# ===========================
+
+# Condição AND
+age_0 = 21
+age_1 = 21
+
+if (age_0 >= 21) and (age_1 >= 21):
+    print("Já pode dirigir, e beber com moderação")
+
+# Condição OR
+age_0 = 15
+acompanhado = True
+
+if (age_0 >= 16) or acompanhado:
+    print("Pode ir no brinquedo!")
+
+# ===========================
+#  Verificando se Valor Está na Lista
+# ===========================
+
+users = ['andrew', 'caroline', 'david']  # Lista de usuários
+user = 'eduardo'
+
+if user not in users:
+    print(f"{user.title()} não está na lista.")
+
+# ===========================
+#  Expressões Booleanas
+# ===========================
+
+game_active = True
+can_edit = False
+
+# ===========================
+#  if e else
+# ===========================
+
+age = 27
+
+# Verificando maioridade
+if age >= 18:
+    print("Você é maior de idade")
+else:
+    print("Você não é maior de idade")
+
+# Várias condições com elif
+if age < 4:
+    print("Você é muito novo")
+elif age < 65:
+    print("Ainda não está velho")
+else:
+    print("Está na terceira idade")
+
+# ===========================
+#  if com Listas
+# ===========================
+
+# Verificando sabores de pizza
+sabor_pizza = ['peperoni', 'extra cheese', 'portuguesa']
+
+if 'peperoni' in sabor_pizza:
+    print("Gostoso")
+if 'extra cheese' in sabor_pizza:
+    print("Delícia")
+if 'portuguesa' in sabor_pizza:
+    print("Ótimo")
+
+# Exibindo os sabores disponíveis
+for sabor in sabor_pizza:
+    print(f"{sabor}")
---
+++ tuplas.py
@@ -0,0 +1,21 @@
+# ===========================
+#  Trabalhando com Tuplas
+# ===========================
+
+# Tuplas são criadas com parênteses ()
+# Tuplas são como listas, porém são constantes (imutáveis)
+
+mega = (6, 4, 18, 27, 55, 56)
+
+# Exibindo a tupla completa
+print(mega)
+
+# ===========================
+#  Percorrendo uma Tupla com Loop
+# ===========================
+
+numero = (1, 2, 3, 4, 5)
+
+# Percorrendo cada elemento da tupla usando um loop for
+for n in numero:
+    print(n)
